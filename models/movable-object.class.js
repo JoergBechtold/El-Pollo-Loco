bca69a134fec6204c4992e511c1bf3236b30cecb
Jörg Bechtold <joergbechtold@gmx.net>
2025-04-30 14:12:45 +0200

    video 10 requestAnimationFrame

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,11 +1,21 @@
 class MovableObject {
     x = 120;
-    y = 400;
+    y = 250;
     img;
-    
+    height = 150;
+    width = 100;    
+    loadeImage(path){
+        this.img = new Image();
+        this.img.src = path;
+    }
 
     moveRight() {
         console.log('Moving Right');
+        
+    }
+
+    moveLeft(){
+
         
     }
 }
