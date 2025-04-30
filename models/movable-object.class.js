85538de569bc8a951753be5967a31bc3020a94e3
Jörg Bechtold <joergbechtold@gmx.net>
2025-04-30 17:07:17 +0200

    video 17

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,10 +1,10 @@
 class MovableObject {
     x = 120;
-    y = 250;
+    y = 280;
     height = 150;
     width = 100;    
     img;
-    
+
     loadeImage(path){
         this.img = new Image();
         this.img.src = path;
