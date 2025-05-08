533aa50aaf9e53a9f2fb1a4976d6d8ccbc7fd69c
Jörg Bechtold <joergbechtold@gmx.net>
2025-05-08 20:11:54 +0200

    math video 19

--- models/world.class.js
+++ models/world.class.js
@@ -37,6 +37,7 @@ class World {
 
     this.ctx.translate(this.camera_x, 0); 
     this.addObjectsToMap(this.level.backgroundObjects);
+    this.addObjectsToMap(this.level.clouds);
 
     this.ctx.translate(-this.camera_x, 0); 
     // ------------Space for fixed objects-----------
@@ -45,7 +46,6 @@ class World {
 
     this.addToMap(this.character);
     this.addObjectsToMap(this.level.enemies);
-    this.addObjectsToMap(this.level.clouds);
 
     this.ctx.translate(-this.camera_x, 0); 
  
